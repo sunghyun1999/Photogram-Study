3e429f7bdd8a525ef30e6bc2d07382d056ba7ff1
sunghyun1999 <32184099@dankook.ac.kr>
2023-04-07 14:44:14 +0900

    회원정보 수정(Ajax) 구현

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -18,11 +19,11 @@ public class UserController {
 
     @GetMapping("/user/{id}/update")
     public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) {
-        System.out.println("세션정보 : " + principalDetails.getUser());
+        /*System.out.println("세션정보 : " + principalDetails.getUser());
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         PrincipalDetails principalDetails2 = (PrincipalDetails) auth.getPrincipal();
-        System.out.println("직접 찾은 세션 정보 : " + principalDetails2.getUser());
+        System.out.println("직접 찾은 세션 정보 : " + principalDetails2.getUser());*/
 
         return "user/update";
     }
