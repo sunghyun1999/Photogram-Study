9ac8ceab88578cfaddd7251addaa7417eebc495c
sunghyun1999 <32184099@dankook.ac.kr>
2023-04-14 17:22:48 +0900

    좋아요 기능 구현

--- src/main/java/com/cos/photogramstart/domain/likes/Likes.java
+++ src/main/java/com/cos/photogramstart/domain/likes/Likes.java
@@ -1,6 +1,6 @@
-package com.cos.photogramstart.domain.image;
+package com.cos.photogramstart.domain.likes;
 
-import com.cos.photogramstart.domain.likes.Likes;
+import com.cos.photogramstart.domain.image.Image;
 import com.cos.photogramstart.domain.user.User;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
@@ -10,36 +10,33 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
-public class Image {
-
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "likes_uk",
+                        columnNames = {"imageId", " userId"}
+                )
+        }
+)
+public class Likes {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private String caption;
-    private String postImageUrl; // 사진을 전송받아서 그 사진을 서버의 특정 폴더에 저장 - DB에 그 저장된 경로를 INSERT
+    @ManyToOne
+    @JoinColumn(name = "imageId")
+    private Image image;
 
     @ManyToOne
-    @JsonIgnoreProperties({"images  "})
+    @JsonIgnoreProperties({"images"})
     @JoinColumn(name = "userId")
     private User user;
-
-    @OneToMany(mappedBy = "image")
-    @JsonIgnoreProperties({"image"})
-    private List<Likes> likes;
-
-    @Transient // DB에 컬럼이 만들어지지 않는다.
-    private boolean likeState;
-
-    @Transient
-    private int likeCount;
 
     private LocalDateTime createDate;
 
